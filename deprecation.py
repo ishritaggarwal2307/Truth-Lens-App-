2734b551725d9c3eb60df3fe80453a74f3572ce4
Ishrit Aggarwal <ishritaggarwal2307@gmail.com>
2026-02-28 22:54:03 +0530

    Add files via upload

--- deprecation.py
+++ deprecation.py
@@ -1,196 +1,124 @@
-from __future__ import annotations
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
 
-import sys
-import threading
+import functools
 import warnings
-from typing import TYPE_CHECKING, Literal
+from inspect import signature
 
-if sys.version_info >= (3, 13):
-    from warnings import deprecated as _deprecated
-else:
-    from typing_extensions import deprecated as _deprecated
+__all__ = ["deprecated"]
 
 
-if TYPE_CHECKING:
-    if sys.version_info >= (3, 11):
-        from typing import LiteralString
-    else:
-        from typing_extensions import LiteralString
+class deprecated:
+    """Decorator to mark a function or class as deprecated.
 
-__all__ = [
-    "AltairDeprecationWarning",
-    "deprecated",
-    "deprecated_static_only",
-    "deprecated_warn",
-]
+    Issue a warning when the function is called/the class is instantiated and
+    adds a warning to the docstring.
 
+    The optional extra argument will be appended to the deprecation message
+    and the docstring. Note: to use this with the default value for extra, put
+    in an empty of parentheses:
 
-class AltairDeprecationWarning(DeprecationWarning): ...
-
-
-def _format_message(
-    version: LiteralString,
-    alternative: LiteralString | None,
-    message: LiteralString | None,
-    /,
-) -> LiteralString:
-    output = f"\nDeprecated since `altair={version}`."
-    if alternative:
-        output = f"{output} Use {alternative} instead."
-    return f"{output}\n{message}" if message else output
-
-
-# NOTE: Annotating the return type breaks `pyright` detecting [reportDeprecated]
-# NOTE: `LiteralString` requirement is introduced by stubs
-def deprecated(
-    *,
-    version: LiteralString,
-    alternative: LiteralString | None = None,
-    message: LiteralString | None = None,
-    category: type[AltairDeprecationWarning] | None = AltairDeprecationWarning,
-    stacklevel: int = 1,
-):  # te.deprecated
-    """
-    Indicate that a class, function or overload is deprecated.
-
-    When this decorator is applied to an object, the type checker
-    will generate a diagnostic on usage of the deprecated object.
+    Examples
+    --------
+    >>> from sklearn.utils import deprecated
+    >>> deprecated()
+    <sklearn.utils.deprecation.deprecated object at ...>
+    >>> @deprecated()
+    ... def some_function(): pass
 
     Parameters
     ----------
-    version
-        ``altair`` version the deprecation first appeared.
-    alternative
-        Suggested replacement class/method/function.
-    message
-        Additional message appended to ``version``, ``alternative``.
-    category
-        If the *category* is ``None``, no warning is emitted at runtime.
-    stacklevel
-        The *stacklevel* determines where the
-        warning is emitted. If it is ``1`` (the default), the warning
-        is emitted at the direct caller of the deprecated object; if it
-        is higher, it is emitted further up the stack.
-        Static type checker behavior is not affected by the *category*
-        and *stacklevel* arguments.
-
-    References
-    ----------
-    [PEP 702](https://peps.python.org/pep-0702/)
+    extra : str, default=''
+          To be added to the deprecation messages.
     """
-    msg = _format_message(version, alternative, message)
-    return _deprecated(msg, category=category, stacklevel=stacklevel)
+
+    # Adapted from https://wiki.python.org/moin/PythonDecoratorLibrary,
+    # but with many changes.
+
+    def __init__(self, extra=""):
+        self.extra = extra
+
+    def __call__(self, obj):
+        """Call method
+
+        Parameters
+        ----------
+        obj : object
+        """
+        if isinstance(obj, type):
+            return self._decorate_class(obj)
+        elif isinstance(obj, property):
+            # Note that this is only triggered properly if the `deprecated`
+            # decorator is placed before the `property` decorator, like so:
+            #
+            # @deprecated(msg)
+            # @property
+            # def deprecated_attribute_(self):
+            #     ...
+            return self._decorate_property(obj)
+        else:
+            return self._decorate_fun(obj)
+
+    def _decorate_class(self, cls):
+        msg = "Class %s is deprecated" % cls.__name__
+        if self.extra:
+            msg += "; %s" % self.extra
+
+        new = cls.__new__
+        sig = signature(cls)
+
+        def wrapped(cls, *args, **kwargs):
+            warnings.warn(msg, category=FutureWarning)
+            if new is object.__new__:
+                return object.__new__(cls)
+
+            return new(cls, *args, **kwargs)
+
+        cls.__new__ = wrapped
+
+        wrapped.__name__ = "__new__"
+        wrapped.deprecated_original = new
+        # Restore the original signature, see PEP 362.
+        cls.__signature__ = sig
+
+        return cls
+
+    def _decorate_fun(self, fun):
+        """Decorate function fun"""
+
+        msg = "Function %s is deprecated" % fun.__name__
+        if self.extra:
+            msg += "; %s" % self.extra
+
+        @functools.wraps(fun)
+        def wrapped(*args, **kwargs):
+            warnings.warn(msg, category=FutureWarning)
+            return fun(*args, **kwargs)
+
+        # Add a reference to the wrapped function so that we can introspect
+        # on function arguments in Python 2 (already works in Python 3)
+        wrapped.__wrapped__ = fun
+
+        return wrapped
+
+    def _decorate_property(self, prop):
+        msg = self.extra
+
+        @property
+        @functools.wraps(prop.fget)
+        def wrapped(*args, **kwargs):
+            warnings.warn(msg, category=FutureWarning)
+            return prop.fget(*args, **kwargs)
+
+        return wrapped
 
 
-def deprecated_warn(
-    message: LiteralString,
-    *,
-    version: LiteralString,
-    alternative: LiteralString | None = None,
-    category: type[AltairDeprecationWarning] = AltairDeprecationWarning,
-    stacklevel: int = 2,
-    action: Literal["once"] | None = None,
-) -> None:
-    """
-    Indicate that the current code path is deprecated.
-
-    This should be used for non-trivial cases *only*. ``@deprecated`` should
-    always be preferred as it is recognized by static type checkers.
-
-    Parameters
-    ----------
-    message
-        Explanation of the deprecated behaviour.
-
-        .. note::
-            Unlike ``@deprecated``, this is *not* optional.
-
-    version
-        ``altair`` version the deprecation first appeared.
-    alternative
-        Suggested replacement argument/method/function.
-    category
-        The runtime warning type emitted.
-    stacklevel
-        How far up the call stack to make this warning appear.
-        A value of ``2`` attributes the warning to the caller
-        of the code calling ``deprecated_warn()``.
-
-    References
-    ----------
-    [warnings.warn](https://docs.python.org/3/library/warnings.html#warnings.warn)
-    """
-    msg = _format_message(version, alternative, message)
-    if action is None:
-        warnings.warn(msg, category=category, stacklevel=stacklevel)
-    elif action == "once":
-        _warn_once(msg, category=category, stacklevel=stacklevel)
-    else:
-        raise NotImplementedError(action)
-
-
-deprecated_static_only = _deprecated
-"""
-Using this decorator **exactly as described**, ensures ``message`` is displayed to a static type checker.
-
-**BE CAREFUL USING THIS**.
-
-See screenshots in `comment`_ for motivation.
-
-Every use should look like::
-
-    @deprecated_static_only(
-        "Deprecated since `altair=5.5.0`. Use altair.other instead.",
-        category=None,
+def _is_deprecated(func):
+    """Helper to check if func is wrapped by our deprecated decorator"""
+    closures = getattr(func, "__closure__", [])
+    if closures is None:
+        closures = []
+    is_deprecated = "deprecated" in "".join(
+        [c.cell_contents for c in closures if isinstance(c.cell_contents, str)]
     )
-    def old_function(*args): ...
-
-If a runtime warning is desired, use `@alt.utils.deprecated` instead.
-
-Parameters
-----------
-message : LiteralString
-    - **Not** a variable
-    - **Not** use placeholders
-    - **Not** use concatenation
-    - **Do not use anything that could be considered dynamic**
-
-category : None
-    You **need** to explicitly pass ``None``
-
-.. _comment:
-    https://github.com/vega/altair/pull/3618#issuecomment-2423991968
----
-"""
-
-
-class _WarningsMonitor:
-    def __init__(self) -> None:
-        self._warned: dict[LiteralString, Literal[True]] = {}
-        self._lock = threading.Lock()
-
-    def __contains__(self, key: LiteralString, /) -> bool:
-        with self._lock:
-            return key in self._warned
-
-    def hit(self, key: LiteralString, /) -> None:
-        with self._lock:
-            self._warned[key] = True
-
-    def clear(self) -> None:
-        with self._lock:
-            self._warned.clear()
-
-
-_warnings_monitor = _WarningsMonitor()
-
-
-def _warn_once(
-    msg: LiteralString, /, *, category: type[AltairDeprecationWarning], stacklevel: int
-) -> None:
-    global _warnings_monitor
-    if msg in _warnings_monitor:
-        return
-    else:
-        _warnings_monitor.hit(msg)
-        warnings.warn(msg, category=category, stacklevel=stacklevel + 1)
+    return is_deprecated
