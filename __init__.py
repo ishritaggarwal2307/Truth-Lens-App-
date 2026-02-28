d71ad610a17f407e14c89c630a5a3b13ca6c8810
Ishrit Aggarwal <ishritaggarwal2307@gmail.com>
2026-02-28 22:30:15 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,145 +1,150 @@
-from ._explanation import Cohorts, Explanation
+"""Configure global settings and get information about the working environment."""
 
-# explainers
-from .explainers import other
-from .explainers._additive import AdditiveExplainer
-from .explainers._coalition import CoalitionExplainer
-from .explainers._deep import DeepExplainer
-from .explainers._exact import ExactExplainer
-from .explainers._explainer import Explainer
-from .explainers._gpu_tree import GPUTreeExplainer
-from .explainers._gradient import GradientExplainer
-from .explainers._kernel import KernelExplainer
-from .explainers._linear import LinearExplainer
-from .explainers._partition import PartitionExplainer
-from .explainers._permutation import PermutationExplainer
-from .explainers._sampling import SamplingExplainer
-from .explainers._tree import TreeExplainer
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
 
-try:
-    # Version from setuptools-scm
-    from ._version import version as __version__
-except ImportError:
-    # Expected when running locally without build
-    __version__ = "0.0.0-not-built"
+# Machine learning module for Python
+# ==================================
+#
+# sklearn is a Python module integrating classical machine
+# learning algorithms in the tightly-knit world of scientific Python
+# packages (numpy, scipy, matplotlib).
+#
+# It aims to provide simple and efficient solutions to learning problems
+# that are accessible to everybody and reusable in various contexts:
+# machine-learning as a versatile tool for science and engineering.
+#
+# See https://scikit-learn.org for complete documentation.
 
-_no_matplotlib_warning = (
-    "matplotlib is not installed so plotting is not available! Run `pip install matplotlib` to fix this."
-)
+import importlib as _importlib
+import logging
+import os
+import random
+
+from sklearn._config import config_context, get_config, set_config
+
+logger = logging.getLogger(__name__)
 
 
-# plotting (only loaded if matplotlib is present)
-def unsupported(*args, **kwargs):
-    raise ImportError(_no_matplotlib_warning)
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.Y.ZaN   # Alpha release
+#   X.Y.ZbN   # Beta release
+#   X.Y.ZrcN  # Release Candidate
+#   X.Y.Z     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = "1.8.0"
 
 
-class UnsupportedModule:
-    def __getattribute__(self, item):
-        raise ImportError(_no_matplotlib_warning)
+# On OSX, we can get a runtime error due to multiple OpenMP libraries loaded
+# simultaneously. This can happen for instance when calling BLAS inside a
+# prange. Setting the following environment variable allows multiple OpenMP
+# libraries to be loaded. It should not degrade performances since we manually
+# take care of potential over-subcription performance issues, in sections of
+# the code where nested OpenMP loops can happen, by dynamically reconfiguring
+# the inner OpenMP runtime to temporarily disable it while under the scope of
+# the outer OpenMP parallel section.
+os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "True")
 
+# Workaround issue discovered in intel-openmp 2019.5:
+# https://github.com/ContinuumIO/anaconda-issues/issues/11294
+os.environ.setdefault("KMP_INIT_AT_FORK", "FALSE")
 
-try:
-    import matplotlib  # noqa: F401
+# `_distributor_init` allows distributors to run custom init code.
+# For instance, for the Windows wheel, this is used to pre-load the
+# vcomp shared library runtime for OpenMP embedded in the sklearn/.libs
+# sub-folder.
+# It is necessary to do this prior to importing show_versions as the
+# later is linked to the OpenMP runtime to make it possible to introspect
+# it and importing it first would fail if the OpenMP dll cannot be found.
+from sklearn import __check_build, _distributor_init  # noqa: E402 F401
+from sklearn.base import clone  # noqa: E402
+from sklearn.utils._show_versions import show_versions  # noqa: E402
 
-    have_matplotlib = True
-except ImportError:
-    have_matplotlib = False
-if have_matplotlib:
-    from . import plots
-    from .plots._bar import bar_legacy as bar_plot
-    from .plots._beeswarm import summary_legacy as summary_plot
-    from .plots._decision import decision as decision_plot
-    from .plots._decision import multioutput_decision as multioutput_decision_plot
-    from .plots._embedding import embedding as embedding_plot
-    from .plots._force import force as force_plot
-    from .plots._force import getjs, initjs, save_html
-    from .plots._group_difference import group_difference as group_difference_plot
-    from .plots._heatmap import heatmap as heatmap_plot
-    from .plots._image import image as image_plot
-    from .plots._monitoring import monitoring as monitoring_plot
-    from .plots._partial_dependence import partial_dependence as partial_dependence_plot
-    from .plots._scatter import dependence_legacy as dependence_plot
-    from .plots._text import text as text_plot
-    from .plots._violin import violin as violin_plot
-    from .plots._waterfall import waterfall as waterfall_plot
-else:
-    bar_plot = unsupported
-    summary_plot = unsupported
-    decision_plot = unsupported
-    multioutput_decision_plot = unsupported
-    embedding_plot = unsupported
-    force_plot = unsupported
-    getjs = unsupported
-    initjs = unsupported
-    save_html = unsupported
-    group_difference_plot = unsupported
-    heatmap_plot = unsupported
-    image_plot = unsupported
-    monitoring_plot = unsupported
-    partial_dependence_plot = unsupported
-    dependence_plot = unsupported
-    text_plot = unsupported
-    violin_plot = unsupported
-    waterfall_plot = unsupported
-    # If matplotlib is available, then the plots submodule will be directly available.
-    # If not, we need to define something that will issue a meaningful warning message
-    # (rather than ModuleNotFound).
-    plots = UnsupportedModule()  # type: ignore
-
-
-# other stuff :)
-from . import datasets, links, utils  # noqa: E402
-from .actions._optimizer import ActionOptimizer  # noqa: E402
-from .utils import approximate_interactions, sample  # noqa: E402
-
-# from . import benchmark
-from .utils._legacy import kmeans  # noqa: E402
-
-# Use __all__ to let type checkers know what is part of the public API.
-__all__ = [
-    "Cohorts",
-    "Explanation",
-    # Explainers
-    "other",
-    "AdditiveExplainer",
-    "DeepExplainer",
-    "ExactExplainer",
-    "Explainer",
-    "GPUTreeExplainer",
-    "GradientExplainer",
-    "KernelExplainer",
-    "LinearExplainer",
-    "PartitionExplainer",
-    "CoalitionExplainer",
-    "PermutationExplainer",
-    "SamplingExplainer",
-    "TreeExplainer",
-    # Plots
-    "plots",
-    "bar_plot",
-    "summary_plot",
-    "decision_plot",
-    "multioutput_decision_plot",
-    "embedding_plot",
-    "force_plot",
-    "getjs",
-    "initjs",
-    "save_html",
-    "group_difference_plot",
-    "heatmap_plot",
-    "image_plot",
-    "monitoring_plot",
-    "partial_dependence_plot",
-    "dependence_plot",
-    "text_plot",
-    "violin_plot",
-    "waterfall_plot",
-    # Other stuff
+_submodules = [
+    "calibration",
+    "cluster",
+    "covariance",
+    "cross_decomposition",
     "datasets",
-    "links",
-    "utils",
-    "ActionOptimizer",
-    "approximate_interactions",
-    "sample",
-    "kmeans",
+    "decomposition",
+    "dummy",
+    "ensemble",
+    "exceptions",
+    "experimental",
+    "externals",
+    "feature_extraction",
+    "feature_selection",
+    "frozen",
+    "gaussian_process",
+    "inspection",
+    "isotonic",
+    "kernel_approximation",
+    "kernel_ridge",
+    "linear_model",
+    "manifold",
+    "metrics",
+    "mixture",
+    "model_selection",
+    "multiclass",
+    "multioutput",
+    "naive_bayes",
+    "neighbors",
+    "neural_network",
+    "pipeline",
+    "preprocessing",
+    "random_projection",
+    "semi_supervised",
+    "svm",
+    "tree",
+    "discriminant_analysis",
+    "impute",
+    "compose",
 ]
+
+__all__ = _submodules + [
+    # Non-modules:
+    "clone",
+    "get_config",
+    "set_config",
+    "config_context",
+    "show_versions",
+]
+
+
+def __dir__():
+    return __all__
+
+
+def __getattr__(name):
+    if name in _submodules:
+        return _importlib.import_module(f"sklearn.{name}")
+    else:
+        try:
+            return globals()[name]
+        except KeyError:
+            raise AttributeError(f"Module 'sklearn' has no attribute '{name}'")
+
+
+def setup_module(module):
+    """Fixture for the tests to assure globally controllable seeding of RNGs"""
+
+    import numpy as np
+
+    # Check if a random seed exists in the environment, if not create one.
+    _random_seed = os.environ.get("SKLEARN_SEED", None)
+    if _random_seed is None:
+        _random_seed = np.random.uniform() * np.iinfo(np.int32).max
+    _random_seed = int(_random_seed)
+    print("I: Seeding RNGs with %r" % _random_seed)
+    np.random.seed(_random_seed)
+    random.seed(_random_seed)
